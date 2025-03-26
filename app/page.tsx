6b3c540c0408421901fc9bccb0be1dbf4c315c56
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-03-26 20:53:43 +0900

    Part 4. 인증구현 - 인증링크 방식 회원가입

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,23 @@
+import LogoutButton from "components/logout-button";
+import { createServerSupabaseClient } from "utils/supabase/server";
+
 export const metadata = {
   title: "Infleangram",
   description: "Instagram clone project",
 };
 
-export default function Home() {
-  return <main>인프런그램</main>;
+export default async function Home() {
+  const supabase = await createServerSupabaseClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return (
+    <main className="w-full h-screen flex flex-col gap-2 items-center justify-center">
+      <h1 className="font-bold text-xl">
+        Welcome {session?.user?.email?.split("@")?.[0]}!
+      </h1>
+      <LogoutButton />
+    </main>
+  );
 }
---
+++ components/layouts/main-layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import SideBar from "components/sidebar";
+
+export default async function MainLayout({ children }) {
+  return (
+    <main className="w-full h-screen flex items-center justify-center">
+      <SideBar />
+      {children}
+    </main>
+  );
+}
