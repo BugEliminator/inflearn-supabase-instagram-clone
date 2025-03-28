b9c9096c9e52550446f77c0ecf999745c4e6d386
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-03-28 13:51:30 +0900

    Part 7. 채팅목록 구현

--- components/chat/ChatScreen.tsx
+++ components/chat/ChatScreen.tsx
@@ -4,22 +4,32 @@ import { Button } from "@material-tailwind/react";
 import Person from "./Person";
 import Message from "./Message";
 import { useRecoilValue } from "recoil";
-import { selectedIndexState } from "utils/recoil/atoms";
+import {
+  selectedUserIdState,
+  selectedUserIndexState,
+} from "utils/recoil/atoms";
+import { useQuery } from "@tanstack/react-query";
+import { getUserById } from "actions/chatActions";
 
-export default function ChatScreen() {
-  const selectedIndex = useRecoilValue(selectedIndexState);
-  return selectedIndex !== null ? (
+export default function ChatScreen({}) {
+  const seletedUserId = useRecoilValue(selectedUserIdState);
+  const seletedUserIndex = useRecoilValue(selectedUserIndexState);
+  const selectedUserQuery = useQuery({
+    queryKey: ["user", seletedUserId],
+    queryFn: () => getUserById(seletedUserId),
+  });
+
+  return selectedUserQuery.data !== null ? (
     <div className="w-full h-screen flex flex-col">
       {/* 액티브 유저 영역 */}
       <Person
-        index={selectedIndex}
-        name={"chanwoo"}
-        userId={undefined}
+        index={seletedUserIndex}
+        name={selectedUserQuery?.data?.email?.split("@")?.[0]}
+        userId={selectedUserQuery?.data?.id}
         onlineAt={new Date().toISOString()}
         isActive={false}
         onChatScreen={true}
       />
-
       {/* 채팅 영역 */}
       <div className="w-full flex-1 flex flex-col p-4 gap-3">
         <Message isFromMe={true} message={"안녕하십니까?"} />
@@ -29,7 +39,6 @@ export default function ChatScreen() {
         <Message isFromMe={true} message={"안녕하십니까?"} />
         <Message isFromMe={false} message={"안녕하십니까?"} />
       </div>
-
       {/* 채팅창 영역 */}
       <div className="flex">
         <input
