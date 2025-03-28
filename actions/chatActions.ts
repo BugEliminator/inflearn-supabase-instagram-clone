5c805139931bdae20fc3ec9e3bbe402297688cac
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-03-28 18:51:52 +0900

    Part 8-1. 실시간 채팅 구현

--- actions/chatActions.ts
+++ actions/chatActions.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { createServerSupabaseAdminClient } from "utils/supabase/server";
+import {
+  createServerSupabaseAdminClient,
+  createServerSupabaseClient,
+} from "utils/supabase/server";
 
 export async function getAllUsers() {
   // Admin으로 가져오는 이유는 모든 유저의 정보를 가져오기 위해
@@ -24,4 +27,59 @@ export async function getUserById(userId) {
     return null;
   }
   return data.user;
+}
+
+export async function sendMessage({ message, chatUserId }) {
+  const supabase = await createServerSupabaseClient();
+
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error || !session.user) {
+    throw new Error("User is not authenticated");
+  }
+
+  const { data, error: sendMessageError } = await supabase
+    .from("message")
+    .insert({
+      message,
+      receiver: chatUserId,
+      sender: session.user.id,
+    });
+
+  if (sendMessageError) {
+    throw new Error(sendMessageError.message);
+  }
+
+  return data;
+}
+
+export async function getAllMessages({ chatUserId }) {
+  const supabase = await createServerSupabaseClient();
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error || !session.user) {
+    throw new Error("User is not authenticated");
+  }
+
+  console.log("🔍 session.user.id", session.user.id);
+  console.log("🔍 chatUserId", chatUserId);
+
+  const { data, error: getMessagesError } = await supabase
+    .from("message")
+    .select("*")
+    .or(`receiver.eq.${chatUserId},receiver.eq.${session.user.id}`)
+    .or(`sender.eq.${chatUserId},sender.eq.${session.user.id}`)
+    .order("created_at", { ascending: true });
+
+  if (getMessagesError) {
+    return [];
+  }
+
+  return data;
 }
--- components/chat/ChatScreen.tsx
+++ components/chat/ChatScreen.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@material-tailwind/react";
+import { Spinner } from "@material-tailwind/react";
 import Person from "./Person";
 import Message from "./Message";
 import { useRecoilValue } from "recoil";
@@ -8,22 +8,71 @@ import {
   selectedUserIdState,
   selectedUserIndexState,
 } from "utils/recoil/atoms";
-import { useQuery } from "@tanstack/react-query";
-import { getUserById } from "actions/chatActions";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getAllMessages, getUserById, sendMessage } from "actions/chatActions";
+import { useEffect, useState } from "react";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
 
 export default function ChatScreen({}) {
-  const seletedUserId = useRecoilValue(selectedUserIdState);
-  const seletedUserIndex = useRecoilValue(selectedUserIndexState);
+  const selectedUserId = useRecoilValue(selectedUserIdState);
+  const selectcedUserIndex = useRecoilValue(selectedUserIndexState);
+  const [message, setMessage] = useState("");
+  const supabase = createBrowserSupabaseClient();
+
   const selectedUserQuery = useQuery({
-    queryKey: ["user", seletedUserId],
-    queryFn: () => getUserById(seletedUserId),
+    queryKey: ["user", selectedUserId],
+    queryFn: () => getUserById(selectedUserId),
   });
+
+  const sendMessageMutation = useMutation({
+    mutationFn: async () => {
+      return sendMessage({
+        message,
+        chatUserId: selectedUserId,
+      });
+    },
+    onSuccess: () => {
+      setMessage("");
+      getAllMessagesQuery.refetch();
+    },
+  });
+
+  const getAllMessagesQuery = useQuery({
+    queryKey: ["messages", selectedUserId],
+    queryFn: () => getAllMessages({ chatUserId: selectedUserId }),
+  });
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("message_postgres_changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "message",
+        },
+        (payload) => {
+          if (payload.eventType === "INSERT" && !payload.errors) {
+            getAllMessagesQuery.refetch();
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      channel.unsubscribe();
+    };
+  }, []);
+
+  console.log("💬 messages", getAllMessagesQuery.data);
+  console.log("🧪 status", getAllMessagesQuery.status);
 
   return selectedUserQuery.data !== null ? (
     <div className="w-full h-screen flex flex-col">
       {/* 액티브 유저 영역 */}
       <Person
-        index={seletedUserIndex}
+        index={selectcedUserIndex}
         name={selectedUserQuery?.data?.email?.split("@")?.[0]}
         userId={selectedUserQuery?.data?.id}
         onlineAt={new Date().toISOString()}
@@ -31,26 +80,38 @@ export default function ChatScreen({}) {
         onChatScreen={true}
       />
       {/* 채팅 영역 */}
-      <div className="w-full flex-1 flex flex-col p-4 gap-3">
-        <Message isFromMe={true} message={"안녕하십니까?"} />
-        <Message isFromMe={true} message={"안녕하십니까?"} />
-        <Message isFromMe={false} message={"안녕하십니까?"} />
-        <Message isFromMe={false} message={"안녕하십니까?"} />
-        <Message isFromMe={true} message={"안녕하십니까?"} />
-        <Message isFromMe={false} message={"안녕하십니까?"} />
+      <div className="w-full overflow-y-scroll flex-1 flex flex-col p-4 gap-3">
+        {getAllMessagesQuery.data?.map((message) => (
+          <Message
+            key={message.id}
+            message={message.message}
+            isFromMe={message.receiver === selectedUserId}
+          />
+        ))}
+        {/* <Message isFromMe={false} message={"반갑습니다."} /> */}
       </div>
       {/* 채팅창 영역 */}
       <div className="flex">
         <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           className="p-3 w-full border-2 border-light-blue-600"
           placeholder="메세지를 입력해주세요."
           type="text"
         />
         <button
+          onClick={() => sendMessageMutation.mutate()}
           className="min-w-20 p-3 bg-light-blue-700 text-white"
           color="light-blue"
         >
-          <span>전송</span>
+          {sendMessageMutation.isPending ? (
+            <Spinner
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            />
+          ) : (
+            <span>전송</span>
+          )}
         </button>
       </div>
     </div>
